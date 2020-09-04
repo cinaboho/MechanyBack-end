07703de78b554b7ffce1aeb608100b9f83ab7086
cinaboho <cinaboho@espol.edu.ec>
2020-09-03 21:54:54 -0500

    Modelo Personas

--- laravel-api/app/Personal_Mecanico.php
+++ laravel-api/app/Personal_Mecanico.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Personal_Mecanico extends Model
 {
-    //
+    ///
 }
+ 
--- laravel-api/app/Personas.php
+++ laravel-api/app/Personas.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Personas extends Model
 {
-    //
+    ///
 }
